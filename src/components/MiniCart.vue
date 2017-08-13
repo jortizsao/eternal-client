06c333fd8aed93071e0d54fe8ec6694aa6d3a370
Javier Ortiz <javi@devgurus.io>
2017-08-13 13:24:45 +0200

    Fix visual defects

--- src/components/MiniCart.vue
+++ src/components/MiniCart.vue
@@ -1,21 +1,21 @@
 <template lang="html">
   <div class="minicart">
-    <div v-if="cart && cart.lineItems > 0">
+    <template v-if="cart && cart.lineItems > 0">
       <button class="not-empty link-your-bag icon-hand-bag">
-        <span class="hidden-xs hidden-sm">{{ $t('miniCart') }}</span>
+        <span class="hidden-xs hidden-sm cart">{{ $t('miniCart') }}</span>
         <span class="cart-item-number">{{cart.lineItems.length}}</span>
       </button>
       <div class="col-xs-12">
         <div class="nav-minicart">
         </div>
       </div>
-    </div>
-    <div v-else>
+    </template>
+    <template v-else>
       <a href="" class="not-empty link-your-bag icon-hand-bag">
-        <span class="hidden-xs hidden-sm">{{ $t('miniCart') }}</span>
+        <span class="hidden-xs hidden-sm cart">{{ $t('miniCart') }}</span>
         <span class="cart-item-number">0</span>
       </a>
-    </div>
+    </template>
   </div>
 
 </template>
