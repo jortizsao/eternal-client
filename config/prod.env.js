cf8771696d9552d52a3d6ed6527f6d783a879242
Javier Ortiz <javier.ortizsaorin@gmail.com>
2017-11-27 23:52:58 +0100

    Add animation in splash image

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,4 +3,5 @@ module.exports = {
   API_URL: `"${process.env.API_URL}"`,
   CMS_ACCESS_TOKEN: `"${process.env.CMS_ACCESS_TOKEN}"`,
   DEFAULT_LANGUAGE: `"${process.env.DEFAULT_LANGUAGE}"`,
+  SPLASH_DURATION: `"${process.env.SPLASH_DURATION}"`,
 }
