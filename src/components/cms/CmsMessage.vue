ef69d447479172bbfefaec29d415177df63cb408
Javi <javier.ortizsaorin@gmail.com>
2018-04-17 19:59:15 +0200

    [[FEAT]] Add CmsMessage component (#24)

---
+++ src/components/cms/CmsMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cms-message">
+    <h4 class="home-suggestions-title">{{blok.title}}</h4>
+    <hr class="home-suggestions-hr"/>
+    <p class="home-suggestions-description">{{blok.description}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ['blok'],
+};
+</script>
