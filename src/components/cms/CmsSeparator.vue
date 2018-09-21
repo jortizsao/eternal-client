2671a8242536ae7281672326ae9262499e92046d
Javier Ortiz <javier.ortizsaorin@gmail.com>
2018-09-21 12:48:30 -0400

    [FEAT] Add cms button links

--- src/components/cms/CmsSeparator.vue
+++ src/components/cms/CmsSeparator.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home-viewall">
-    <a :href="blok.link" class="home-viewall-btn">
-      {{blok.message}}
-    </a>
+    <router-link :to="urlLink" class="home-viewall-btn">{{blok.message}}</router-link>
     <hr class="home-viewall-hr">
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
   props: ['blok'],
+  computed: {
+    urlLink() {
+      return `/${this.language}/${this.blok.link}`;
+    },
+    ...mapState('general', ['language']),
+  },
 };
 </script>
